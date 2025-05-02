e6307f52f4466f3439e84f23a702f7eba6d7fb3e
Keith Rieck <KeithRieck@gmail.com>
2025-05-01 20:23:07 -0500

    Reworked main.py to use classes

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# Things to do before publishing this
+
+* On Driver Station, there appears to be two items listed.  Can this be fixed?  Is there too much stuff in the report descriptor.
+    * Retest with multiple controllers plugged into driver station
+    * Consider editing the report descriptor manually
+    * Consider harvesting the descriptor  bits and rewroking them.
+* On Driver Station, it is identified as "CircuitPython".  Can we change the name?
+* Test on Metro RP2350
+* Test on Pico 2
--- main.py
+++ main.py
@@ -1,44 +1,77 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# You must add a gamepad HID device inside your boot.py file
-# in order to use this example.
-# See this Learn Guide for details:
-# https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/hid-devices#custom-hid-devices-3096614-9
-
 import board
 import digitalio
-import analogio
 import usb_hid
-
 from hid_gamepad import Gamepad
 
-DEBUG = False
+
+class GamepadButton:
+    "Represents a gamepad button, and its corresponding hardware pin."
+
+    def __init__(self, button_num: int, pin: int, button_name: str = None):
+        self.button_num = button_num
+        self.pin = pin
+        self.button_name = button_name
+        self.dio = digitalio.DigitalInOut(pin)
+        self.dio.direction = digitalio.Direction.INPUT
+        self.dio.pull = digitalio.Pull.UP
+
+    def is_pressed(self) -> bool:
+        return not self.dio.value
+    
+    def __repr__(self) -> str:
+        button_label =  f",'{button.button_name}'" if button.button_name is not None else ""
+        return f"GamepadButton({self.button_num},{self.pin}{button_label})"
+
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+
+class ButtonSet:
+    """
+    Tracks the buttons that are currently pressed.
+    This class exists primarily for debugging and monitoring.
+    Yes, it's kind of overcomplicated for what it does.
+    """
+
+    def __init__(self):
+        self.buttons_set: set[int] = set()
+
+    def add(self, button: GamepadButton):
+        if button.button_num not in self.buttons_set:
+            self._log("press  ", button)
+        self.buttons_set.add(button.button_num)
+
+    def remove(self, button: GamepadButton):
+        if button.button_num in self.buttons_set:
+            self._log("release", button)
+            self.buttons_set.remove(button.button_num)
+
+    def _log(self, action: str, button: GamepadButton):
+        global led
+        print(f" {action} {button}", end="\n")
+        led.value = len(self.buttons_set) == 0
+    
+    def __repr__(self) -> str:
+        return f"ButtonSet({self.buttons_set})"
+
 
 gp = Gamepad(usb_hid.devices)
 
-# Create some buttons. The physical buttons are connected
-# to ground on one side and these and these pins on the other.
-button_pins = (board.GP9, board.GP10, board.GP22, board.GP21)
+button_list = [
+    GamepadButton(1, board.GP9),
+    GamepadButton(2, board.GP10),
+    GamepadButton(3, board.GP22),
+    GamepadButton(4, board.GP21),
+]
+button_set = ButtonSet()
 
-# Map the buttons to button numbers on the Gamepad.
-# gamepad_buttons[i] will send that button number when buttons[i]
-# is pushed.
-gamepad_buttons = (1, 2, 3, 4)
-
-buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
-for button in buttons:
-    button.direction = digitalio.Direction.INPUT
-    button.pull = digitalio.Pull.UP
 
 while True:
-    # Buttons are grounded when pressed (.value = False).
-    for i, button in enumerate(buttons):
-        gamepad_button_num = gamepad_buttons[i]
-        if button.value:
-            gp.release_buttons(gamepad_button_num)
-            if DEBUG: print(" release", gamepad_button_num, end="\n")
+    for button in button_list:
+        if button.is_pressed():
+            gp.press_buttons(button.button_num)
+            button_set.add(button)
         else:
-            gp.press_buttons(gamepad_button_num)
-            if DEBUG: print(" press", gamepad_button_num, end="\n")
-
+            gp.release_buttons(button.button_num)
+            button_set.remove(button)
